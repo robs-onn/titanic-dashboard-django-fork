266546bb3b8782d60e6294d19863e3ddabc09647
Robson Junior <robsonwtj@gmail.com>
2025-11-02 12:06:51 -0300

    Views atualizadas para utilizar banco de dados e algumas refatorações

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
+#username: test_user
+#pass: a123456b
+
+
 # Create your tests here.
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,52 +1,84 @@
 from django.shortcuts import render
 import pandas as pd
 from dashboard.models import TitanicPassenger
-
+from django.db.models import Count, Sum, F
 
 # Create your views here.
 def index(request):
     context = {}
-    df = pd.read_csv("static/data/titanic.csv")
+    #df = pd.read_csv("static/data/titanic.csv")
 
+    #total_passenger = df.shape[0]
     total_passenger = TitanicPassenger.objects.count()
     context["total_passenger"] = total_passenger
 
+    #total_male = df[df["Sex"] == "male"].shape[0]
     total_male = TitanicPassenger.objects.filter(sex="male").count()
     context["total_male"] = total_male
+    
     context["total_female"] = total_passenger - total_male
-
+    #total_female = TitanicPassenger.objects.filter(sex="female").count()
+    #context["total_female"] = total_female
+    
+    #context["total_fare"] = "$ " + str(int(df["Fare"].sum() // 1000)) + "K"
     total_fare = sum(TitanicPassenger.objects.values_list("fare", flat=True))
     context["total_fare"] = "$ " + str(int(total_fare // 1000)) + "K"
 
-    total_survived = df[df["Survived"] == 1].shape[0]
+    #total_survived = df[df["Survived"] == 1].shape[0]
+    total_survived = TitanicPassenger.objects.filter(survived=1).count()
     context["total_survived"] = total_survived
     context["survived_rate"] = round(total_survived / total_passenger * 100, 2)
 
-    classes = sorted(df["Pclass"].unique().tolist())
+    #classes = sorted(df["Pclass"].unique().tolist())
+    classes_lista = TitanicPassenger.objects.values_list('pclass', flat=True).distinct().order_by('pclass')
+    classes = list(classes_lista)
     context["classes"] = classes
 
-    count_by_class = list(df["Pclass"].value_counts().loc[classes])
+    #count_by_class = list(df["Pclass"].value_counts().loc[classes])
+    counts_q = TitanicPassenger.objects.values('pclass').annotate(count=Count('id')).order_by('pclass')
+    count_by_class = [item['count'] for item in counts_q]
     context["count_by_class"] = count_by_class
 
-    survived_by_class = df.groupby("Pclass")["Survived"].sum().loc[classes].tolist()
+    #survived_by_class = df.groupby("Pclass")["Survived"].sum().loc[classes].tolist()
+    survived_q = TitanicPassenger.objects.values('pclass').annotate(total_survived=Sum('survived')).order_by('pclass')
+    survived_by_class = [item['total_survived'] for item in survived_q]
     context["survived_by_class"] = survived_by_class
 
-    df["Died"] = df["Survived"].map(lambda x: abs(x - 1))
-    died_by_class = df.groupby("Pclass")["Died"].sum().loc[classes].tolist()
+    #df["Died"] = df["Survived"].map(lambda x: abs(x - 1))
+    #died_by_class = df.groupby("Pclass")["Died"].sum().loc[classes].tolist()
+    died_by_class = [total - survived for total, survived in zip(count_by_class, survived_by_class)]
     context["died_by_class"] = died_by_class
 
-    top_10_fares = df.nlargest(10, "Fare")[["Name", "Fare"]].to_dict(orient="records")
-    context["top_10_fares"] = top_10_fares
+    #top_10_fares = df.nlargest(10, "Fare")[["Name", "Fare"]].to_dict(orient="records")
+    top_10_fares = TitanicPassenger.objects.order_by('-fare').values(Name=F('name'), Fare=F('fare'))[:10]
+    context["top_10_fares"] = list(top_10_fares)
 
-    df.dropna(subset=["Embarked"], inplace=True)
-    df = df[df["Survived"] == 1]
-    embarked_by_class = df.groupby(["Embarked", "Pclass"]).size().unstack(fill_value=0)
+    # df.dropna(subset=["Embarked"], inplace=True)
+    # df = df[df["Survived"] == 1]
+    # embarked_by_class_df = df.groupby(["Embarked", "Pclass"]).size().unstack(fill_value=0) # Renomeado p/ clareza
+    
+    # ports = embarked_by_class_df.index.tolist()
+    # context["ports"] = ports
 
-    ports = embarked_by_class.index.tolist()
+    # embarked_by_class = embarked_by_class_df.loc[ports, classes].values.tolist()[::-1]
+    # context["embarked_by_class"] = embarked_by_class
+
+    ports_q = TitanicPassenger.objects.exclude(embarked=None).values_list('embarked', flat=True).distinct().order_by('embarked')
+    ports = list(ports_q)
     context["ports"] = ports
+    
+    embarked_q = TitanicPassenger.objects.exclude(embarked=None).filter(survived=1).values('embarked', 'pclass').annotate(count=Count('id')).order_by('embarked', 'pclass')
 
-    embarked_by_class = embarked_by_class.loc[ports, classes].values.tolist()[::-1]
-
+    pivot_data = {port: {p_class: 0 for p_class in classes} for port in ports}
+    
+    for item in embarked_q : pivot_data[item['embarked']][item['pclass']] = item['count']
+    
+    embarked_by_class_list = []
+    for port in ports:
+        row = [pivot_data[port][p_class] for p_class in classes]
+        embarked_by_class_list.append(row)
+        
+    embarked_by_class = embarked_by_class_list[::-1]
     context["embarked_by_class"] = embarked_by_class
 
     return render(request, "dashboard/index.html", context=context)
